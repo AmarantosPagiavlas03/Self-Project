317839a668aa55cc655e9fd6e74e970f3cf19a46
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-02-08 16:05:30 +0100

    test

--- main.py
+++ main.py
@@ -513,7 +513,7 @@ def send_email_code(to_email, code):
     )
     try:
         sg = SendGridAPIClient(SENDGRID_API_KEY)
-        st.write(st.secrets["SENDGRID_API_KEY"])
+        st.write(st.secrets["SENDGRID_API_KEY"][0])
         response = sg.send(message)
         # Optional: You can log response.status_code or response.body if needed
     except Exception as e:
