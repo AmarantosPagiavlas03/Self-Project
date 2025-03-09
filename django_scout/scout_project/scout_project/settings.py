90764339a7e80c196e2e1eba44fcd76c196bc55a
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-03-09 13:32:25 +0100

    implementing mysql database

--- django_scout/scout_project/scout_project/settings.py
+++ django_scout/scout_project/scout_project/settings.py
@@ -88,11 +88,18 @@ WSGI_APPLICATION = 'scout_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'scout_db',
+        'USER': 'Amarantos',
+        'PASSWORD': 'Amarantos2003!',  # Default MariaDB root password
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
