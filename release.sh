33fc1ff63f3c12da8afce098b65d70a5debc959a
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-03-10 11:30:41 +0100

    Cleaning up

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-python djanco_scout/scout_project/manage.py migrate
-python djanco_scout/scout_project/manage.py makesuperuser
+python django_scout/scout_project/manage.py migrate
+python django_scout/scout_project/manage.py makesuperuser
