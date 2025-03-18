3cc7b2f311b9eeaf94d7ae9b58dc2889f9f7aceb
AmarantosPagiavlas2003 <a.e.pagiavlas@student.vu.nl>
2025-03-18 18:59:56 +0100

    reverting to sqlite

--- django_scout/scout_project/scout_project/settings.py
+++ django_scout/scout_project/scout_project/settings.py
@@ -98,14 +98,11 @@ WSGI_APPLICATION = 'scout_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', 
-            'mysql://Amarantos:Amarantos2003!@localhost:3306/scout_db'
-        ),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
